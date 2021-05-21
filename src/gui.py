8539c8001c66433ea854b90b30acc3cdc3be574a
federicogarzarelli <federico.garzarelli@gmail.com>
2021-05-21 16:52:06 +0200

    testing db

--- src/gui.py
+++ src/gui.py
@@ -7,17 +7,17 @@ import os, sys
 
 app = MultiApp()
 
-wd = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-logo_path = utils.find('logo_small.png', wd)
-#print(logo_path)
-#logo = Image.open(logo_path)
-
-header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(utils.img_to_bytes(logo_path))
-st.markdown(header_html, unsafe_allow_html=True)
-
-#st.image(logo)
-
-utils.delete_output_first()
+# wd = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# logo_path = utils.find('logo_small.png', wd)
+# #print(logo_path)
+# #logo = Image.open(logo_path)
+#
+# header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(utils.img_to_bytes(logo_path))
+# st.markdown(header_html, unsafe_allow_html=True)
+#
+# #st.image(logo)
+#
+# utils.delete_output_first()
 
 st.markdown("""
 # Backtest engine
